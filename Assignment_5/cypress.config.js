f31aef8e4115d60f7916530693bb513fa804e6a1
akash01847 <124980188+akash01847@users.noreply.github.com>
2023-05-02 17:18:17 +0530

    Update cypress.config.js

--- Assignment_5/cypress.config.js
+++ Assignment_5/cypress.config.js
@@ -1,31 +1,3 @@
-// const path = require("path");
-// const { merge } = require("mochawesome-merge");
-// const { create } = require("mochawesome-report-generator");
-
-// module.exports = {
-//   // ...
-//   // ...
-//   reporters: [
-//     "mochawesome",
-//     {
-//       reporterName: "mochawesome",
-//       options: {
-//         reportDir: path.resolve(__dirname, "cypress/reports/mocha"),
-//         overwrite: false,
-//         html: false,
-//         json: true,
-//       },
-//     },
-//   ],
-
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//       require('cypress-mochawesome-reporter/plugin')(on);
-//     },
-//   },
-// };
-
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
